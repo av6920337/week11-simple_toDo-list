f17c04040b85ac9070f15bc88da8f1b96201b9e9
av6920337 <av6920337@gmail.com>
2024-06-18 02:52:09 +0200

    The practical work was completed

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,6 +7,18 @@ function addTask () {
     itemLi.textContent=myInput.value;
     mainList.append(itemLi);
     myInput.value='';
+
+    itemLi.addEventListener('click', function() {
+        this.classList.toggle('checked'); // Это добавит или уберет класс при каждом клике.
+    });
+    
+    let span=document.createElement('span');
+        span.textContent='\u00d7';
+        itemLi.appendChild(span);
+    
+    span.addEventListener('click', function(){
+        this.parentElement.remove();//удаляет родительский элемент.
+    });
 } 
 function handleKeyPress(e){
     let key=e.keyCode || e.which;
